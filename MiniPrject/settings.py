8243bba9359fc7f3cdbd5091b371e983a6602f64
Bhanderi Meet <55754435+meetbhanderi@users.noreply.github.com>
2019-09-24 23:02:00 +0530

    Update settings.py

--- MiniPrject/settings.py
+++ MiniPrject/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '4uezn56&4)5@_^#xjh(&cwduz!edlnx%ac&7%o*v4s=7y7^20c'
+SECRET_KEY = 'YOUR SECRET KEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
